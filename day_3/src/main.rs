9dfc139d313020f5e3234333ef05d3913aabd50d
CadenQ <cadenq1@gmail.com>
2025-12-03 12:51:54 -0500

    Started part 2, but definitely didn't get it correct, will need to work on this more later.

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,22 +5,25 @@ fn main() -> std::io::Result<()> {
 
     let banks = contents.lines();
     let mut count_part1 = 0;
+    let mut count_part2 = 0;
     banks.for_each(|r| {
         let bank: Vec<char> = r.to_string().chars().collect();
         count_part1 += part1(&bank);
+        count_part2 += part2(&bank);
     });
     println!("count part 1: {}", count_part1);
+    println!("count part 2: {}", count_part2);
 
     Ok(())
 }
 
 fn part1(bank: &Vec<char>) -> i32 {
-    let mut top: (i32, char) = (-1, ' ');
+    let mut top: (usize, char) = (0, ' ');
     let mut second: char = ' ';
 
     for (i, c) in bank[..bank.len()-1].iter().enumerate() {
         if (*c) as u32 > top.1 as u32 {
-            top = (i as i32, *c);
+            top = (i, *c);
         }
     }
 
@@ -31,4 +34,24 @@ fn part1(bank: &Vec<char>) -> i32 {
     }
 
     format!("{}{}", top.1, second).parse().unwrap()
+}
+
+fn part2(bank: &Vec<char>) -> i64 {
+    let mut digits: [(usize, char); 12] = [(0, ' '); 12];
+
+    for cur_digit in 0..digits.len() {
+        // Iterate through the remaining characters in the bank to find next highest value
+        for (i, c) in bank[(digits[cur_digit].0 as usize)..(12 - digits[cur_digit].0 as usize)].iter().enumerate() {
+            if (*c) as u32 > digits[cur_digit].1 as u32 {
+                digits[cur_digit] = (i, *c);
+            }
+        }   
+    }
+
+    let mut final_num = String::new();
+    let _ = digits.map(|f| {
+        final_num.push(f.1);
+    });
+    
+    final_num.parse().unwrap()
 }
