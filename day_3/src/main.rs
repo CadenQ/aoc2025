20d142c2e4979ce08cd8bb4469c2f143b3bbc253
CadenQ <cadenq1@gmail.com>
2025-12-03 12:21:14 -0500

    Day 3 part 1 finished

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -3,8 +3,32 @@ use std::fs;
 fn main() -> std::io::Result<()> {
     let contents = fs::read_to_string("input.txt")?;
 
-    let ranges = contents.lines();
-    println!("Hello, world!");
+    let banks = contents.lines();
+    let mut count_part1 = 0;
+    banks.for_each(|r| {
+        let bank: Vec<char> = r.to_string().chars().collect();
+        count_part1 += part1(&bank);
+    });
+    println!("count part 1: {}", count_part1);
 
     Ok(())
+}
+
+fn part1(bank: &Vec<char>) -> i32 {
+    let mut top: (i32, char) = (-1, ' ');
+    let mut second: char = ' ';
+
+    for (i, c) in bank[..bank.len()-1].iter().enumerate() {
+        if (*c) as u32 > top.1 as u32 {
+            top = (i as i32, *c);
+        }
+    }
+
+    for c in bank[(top.0 as usize)+1..].iter() {
+        if (*c) as u32 > second as u32 {
+            second = *c;
+        }
+    }
+
+    format!("{}{}", top.1, second).parse().unwrap()
 }
